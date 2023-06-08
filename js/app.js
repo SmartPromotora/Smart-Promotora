b07349e59fdcd7b17b73538b092901f7aa7a638a
Wanderson.jpeg <81868579+WandersonSantoos@users.noreply.github.com>
2023-06-08 14:41:05 -0300

    Corrigido alguns bugs e adicionado um animação
    Falta corrigir inúmeras coisas, está versão é apenas para teste

--- js/app.js
+++ js/app.js
@@ -44,6 +44,38 @@ class MobileNavbar {
   );
   mobileNavbar.init();
 
+
+/*========== Animações ==========*/
+$(document).ready(function(){
+    $(window).scroll(function(){
+        if(this.scrollY > 20){
+            $('.navbar').addClass("sticky");
+        }else{
+            $('.navbar').removeClass("sticky");
+        }
+        if(this.scrollY > 500){
+            $('.scroll-up-btn').addClass("show");
+        }else{
+            $('.scroll-up-btn').removeClass("show");
+        }
+    });
+     $('.scroll-up-btn').click(function(){
+         $('html').animate({scrollTop: 0});
+     });
+
+     var typed = new Typed(".typing", {
+         strings:["SEGURANÇA", "CONFORTO", "AGILIDADE"],
+         typeSpeed:100,
+         backSpeed:60,
+         loop:true
+     });
+
+    $('.menu-btn').click(function(){
+        $('.navbar .menu').toggleClass("active");
+        $('.menu-btn i').toggleClass("active");
+    });
+});
+
 /*========== Modelo de serviços ==========*/
 const modalViews = document.querySelectorAll('.services-modal'),
       modalBtns = document.querySelectorAll('.services-button'),
