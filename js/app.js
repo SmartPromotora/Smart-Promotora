e3c0951249e1bcc138302c764b5d9abe78fe1571
Wanderson.jpeg <81868579+WandersonSantoos@users.noreply.github.com>
2023-06-10 14:21:51 -0300

    Algumas melhorias foram adicionadas

--- js/app.js
+++ js/app.js
@@ -46,6 +46,24 @@ class MobileNavbar {
 
 
 /*========== Animações ==========*/
+
+// Animação crescente 
+let valueDisplays = document.querySelectorAll(".num");
+let interval = 4000;
+
+valueDisplays.forEach((valueDisplay) => {
+  let startValue = 0;
+  let endValue = parseInt(valueDisplay.getAttribute("data-val"));
+  let duration = Math.floor(interval / endValue);
+  let counter = setInterval(function () {
+    startValue +=1;
+    valueDisplay.textContent = startValue;
+    if (startValue == endValue) {
+      clearInterval(counter);
+    }
+  }, duration);
+});
+
 $(document).ready(function(){
     $(window).scroll(function(){
         if(this.scrollY > 20){
